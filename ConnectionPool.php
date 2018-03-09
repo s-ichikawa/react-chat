185e49550b0f9b69c626ea4c1e31390cfa99f4ec
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2018-03-09 22:08:18 +0900

    update 7.1

--- ConnectionPool.php
+++ ConnectionPool.php
@@ -19,6 +19,10 @@ class ConnectionPool
         $this->connection = new \SplObjectStorage();
     }
 
+    /**
+     * Connectionを追加する
+     * @param ConnectionInterface $connection
+     */
     public function add(ConnectionInterface $connection)
     {
         $connection->write("Hi\n");
@@ -29,6 +33,11 @@ class ConnectionPool
         $this->sendAll("New User enters the chat\n", $connection);
     }
 
+    /**
+     * 接続したConnectionに対してイベントを設定する
+     *
+     * @param ConnectionInterface $connection
+     */
     protected function initEvents(ConnectionInterface $connection)
     {
         $connection->on('data', function ($data) use ($connection) {
@@ -41,6 +50,11 @@ class ConnectionPool
         });
     }
 
+    /**
+     * Poolされている全てのConnectionに対して書き込みを行う
+     * @param $data
+     * @param ConnectionInterface $except
+     */
     protected function sendAll($data, ConnectionInterface $except)
     {
         foreach ($this->connection as $conn) {
--- server.php
+++ server.php
@@ -6,7 +6,7 @@ require_once './ConnectionPool.php';
 
 $loop = \React\EventLoop\Factory::create();
 
-$host = getenv('HOSTNAME');
+$host = getenv('HOSTNAME') ?: '127.0.0.1';
 $socket = new \React\Socket\Server(gethostbyname($host) . ':8080', $loop);
 $pool = new ConnectionPool();
 
